1bb74595955ca78ae4bc817071fb7d8696f08cdf
hahnicity <grehm87@gmail.com>
2015-05-23 01:43:05 -0700

    add new methods we can use for data population
    frustrating

--- redcrab/main.py
+++ redcrab/main.py
@@ -3,6 +3,7 @@ from getpass import getpass
 
 from praw import Reddit
 
+from redcrab import constants
 from redcrab.crawler import set_submission
 from redcrab.postgres import create_connection
 
@@ -25,13 +26,17 @@ def build_parser():
         required=True,
         help="The user agent to make requests with"
     )
+    parser.add_argument(
+        "--method",
+        required=True,
+        choices=constants.METHODS,
+        help="Choose the way to get reddit submissions"
+    )
     return parser
 
 
-def single_threaded_impl(subreddit, db_connection, limit):
-    # For now just hardcode it to parse top comments
-    top = subreddit.get_top(limit=None)
-    for sub, comments in [(sub, sub.comments) for sub in subreddit.get_top(limit=limit)]:
+def single_threaded_impl(subreddit, db_connection, limit, method):
+    for sub, comments in [(sub, sub.comments) for sub in getattr(subreddit, method)(limit=limit)]:
         set_submission(db_connection, sub, comments)
     db_connection.close()
 
@@ -44,4 +49,4 @@ def main():
     if args.username:
         password = getpass("Enter reddit password: ")
         reddit.login(args.username, password)
-    single_threaded_impl(subreddit, db_connection, args.sub_limit)
+    single_threaded_impl(subreddit, db_connection, args.sub_limit, args.method)
